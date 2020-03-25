eece5746953362bfc080c3e075e6454740f1f830
darkv314 <60781992+darkv314@users.noreply.github.com>
2020-03-24 21:37:18 -0400

    bug fix

--- base_datos_SA/SistemaAcademico.sql
+++ base_datos_SA/SistemaAcademico.sql
@@ -50,6 +50,7 @@ UNLOCK TABLES;
 -- Table structure for table `biblioteca`
 --
 
+
 DROP TABLE IF EXISTS `biblioteca`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
